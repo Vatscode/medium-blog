8df28a5e2c4e7744974a4baf23bc27c6b70ae799
Vatscode <becomeharkirat1112@gmail.com>
2025-04-14 08:25:26 -0300

    common-created

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -2,6 +2,10 @@ import { Hono, Context } from 'hono';
 
 export function initMiddleware(app: Hono) {
     app.use('/api/v1/blog/*', async (c: Context, next: () => void) => {
+        //get the header
+        //verify the header
+        //if the header is correct, we can proceed
+        //if not , we return the use a 403 status code 
         const header = c.req.header("authorization") || "";
         // Bearer token => ["Bearer", "token"];
         const token = header.split(" ")[1]
